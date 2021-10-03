3b976267256e97f578113184705768770256215a
Anton Prokofiev <prokofiev1@outlook.de>
2021-10-03 18:44:29 +0200

    Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tse",
+    version="0.0.1",
+    author="Anton Prokofiev",
+    author_email="prokofiev1@gmx.de",
+    packages=["tse"],
+)
